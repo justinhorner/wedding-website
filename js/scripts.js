86ed19aafd0f9d2a44d516b2759ab965cf9fe8a4
Justin Horner <mail@justinhorner.me>
2019-01-10 07:07:11 -0700

    hero and map updates

--- js/scripts.js
+++ js/scripts.js
@@ -251,7 +251,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 33.3653514, lng: -111.7755532};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
