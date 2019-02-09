eda447597844e9170b5dbb548bed18538ed6b3ef
Justin Horner <mail@justinhorner.me>
2019-02-09 13:24:33 -0700

    testing removal of code

--- js/scripts.js
+++ js/scripts.js
@@ -213,22 +213,31 @@ $(document).ready(function () {
         var data = $(this).serialize();
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://script.google.com/macros/s/AKfycbz7R8UcnlybES8etfFvdjpWjye4tOy5F9jqCtGN/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html('');
-                    $('#rsvp-modal').modal('show');
-                })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                });
-        }
+        $.post('https://script.google.com/macros/s/AKfycbz7R8UcnlybES8etfFvdjpWjye4tOy5F9jqCtGN/exec', data)
+        .done(function (data) {
+            console.log(data);
+            $('#alert-wrapper').html('');
+            $('#rsvp-modal').modal('show');
+        })
+        .fail(function (data) {
+            console.log(data);
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+        });
+        // if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+        //     && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        //     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        // } else {
+        //     $.post('https://script.google.com/macros/s/AKfycbz7R8UcnlybES8etfFvdjpWjye4tOy5F9jqCtGN/exec', data)
+        //         .done(function (data) {
+        //             console.log(data);
+        //             $('#alert-wrapper').html('');
+        //             $('#rsvp-modal').modal('show');
+        //         })
+        //         .fail(function (data) {
+        //             console.log(data);
+        //             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+        //         });
+        // }
     });
 
 });
