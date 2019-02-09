49021719a35ad7bdd219d3f7351c22e866f26c0d
Justin Horner <mail@justinhorner.me>
2019-02-09 13:31:08 -0700

    updated minified code, removed commented

--- js/scripts.js
+++ js/scripts.js
@@ -223,21 +223,6 @@ $(document).ready(function () {
             console.log(data);
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
         });
-        // if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-        //     && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-        //     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        // } else {
-        //     $.post('https://script.google.com/macros/s/AKfycbz7R8UcnlybES8etfFvdjpWjye4tOy5F9jqCtGN/exec', data)
-        //         .done(function (data) {
-        //             console.log(data);
-        //             $('#alert-wrapper').html('');
-        //             $('#rsvp-modal').modal('show');
-        //         })
-        //         .fail(function (data) {
-        //             console.log(data);
-        //             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-        //         });
-        // }
     });
 
 });
